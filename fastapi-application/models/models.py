da1c17605e49a5a067ceccc11f55514c07182f50
Alexandr <sashapliskov@gmail.com>
2025-01-30 19:48:43 +0300

    Create declarative base

---
+++ fastapi-application/models/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import declared_attr, DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    @declared_attr.directive
+    def __tabllename__(cls):
+        return f"{cls.__name__.lower()}s"
