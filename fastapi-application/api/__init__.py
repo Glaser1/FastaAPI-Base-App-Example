3a2dae8310840af8d93ccfc94733cf46a4d94c2f
Alexandr <sashapliskov@gmail.com>
2025-02-05 17:37:28 +0300

    Add api_v1 package and router and its config, set up imports

--- fastapi-application/api/__init__.py
+++ fastapi-application/api/__init__.py
@@ -1,3 +1,7 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+from config import settings
+from api.api_v1 import router as api_v1_router
+
+router = APIRouter(prefix=settings.api.prefix)
+router.include_router(api_v1_router)
