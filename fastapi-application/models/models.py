d469e40702009a75205e0c5da1cb88b4c75eeeb3
Alexandr <sashapliskov@gmail.com>
2025-02-05 16:17:47 +0300

    Add naming convention to base model metadata

--- fastapi-application/models/models.py
+++ fastapi-application/models/models.py
@@ -1,8 +1,13 @@
 from sqlalchemy.orm import declared_attr, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import UniqueConstraint, MetaData
+
+from config import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
@@ -13,3 +18,7 @@ class Base(DeclarativeBase):
 
 class User(Base):
     username: Mapped[str] = mapped_column(unique=True)
+    foo: Mapped[int]
+    bar: Mapped[int]
+
+    __table_args__ = (UniqueConstraint("foo", "bar"),)
